fdedd9452f6ec9811ed32f3059900070a4345e68
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-03-05 22:27:47 -0300

    test: signup controller

---
+++ src/presentation/controllers/signup-controller/signup.spec.ts
@@ -0,0 +1,20 @@
+import { SignUpController } from "./signup";
+
+describe('Signup Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirm: 'any_password'
+      }
+    }
+
+    const httResponse = sut.handle(httpRequest);
+
+    expect(httResponse.statusCode).toBe(400);
+  })
+})
