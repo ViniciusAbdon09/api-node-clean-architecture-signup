30a1a04e3817e75bc1f6ea55ef4ae6d5d903b7f9
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-03-05 23:01:47 -0300

    test: ensure SignUpController return an error if no email is provided

--- src/presentation/controllers/signup-controller/signup.spec.ts
+++ src/presentation/controllers/signup-controller/signup.spec.ts
@@ -6,7 +6,6 @@ describe('Signup Controller', () => {
 
     const httpRequest = {
       body: {
-        name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirm: 'any_password'
@@ -17,5 +16,22 @@ describe('Signup Controller', () => {
 
     expect(httResponse.statusCode).toBe(400);
     expect(httResponse.body).toEqual(new Error('Missing param: name'));
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirm: 'any_password'
+      }
+    }
+
+    const httResponse = sut.handle(httpRequest);
+
+    expect(httResponse.statusCode).toBe(400);
+    expect(httResponse.body).toEqual(new Error('Missing param: email'));
   })
 })
