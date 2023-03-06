c1e3acd50cf58796b9d0af28ecf247ea818b443c
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-03-06 20:02:53 -0300

    refactor: move serverError to a helper method

--- src/presentation/controllers/signup-controller/signup.ts
+++ src/presentation/controllers/signup-controller/signup.ts
@@ -1,7 +1,6 @@
 import { InvalidParamError } from "../../error/invalid-param-error";
 import { MissingParamError } from "../../error/missing-param-error"
-import { ServerError } from "../../error/server-error";
-import { badRequest } from "../../helpers/http-helper"
+import { badRequest, serverError } from "../../helpers/http-helper"
 import { Controller } from "../../protocols/controller";
 import { EmailValidator } from "../../protocols/emailValidator";
 import { HttpRequest, HttpResponse } from "../../protocols/http"
@@ -35,10 +34,7 @@ export class SignUpController implements Controller {
         body: {}
       }
     } catch (e) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
