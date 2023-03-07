f6e00cdb0897254bdbea7524a4f4e46cd0424414
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-03-06 21:20:08 -0300

    feat: ensure SignUpController calls addAccount with correct values

--- src/presentation/controllers/signup-controller/signup.ts
+++ src/presentation/controllers/signup-controller/signup.ts
@@ -1,12 +1,15 @@
+import { AddAccount } from "../../../domain/useCases/addAccount/addAccount";
 import { InvalidParamError, MissingParamError } from "../../error";
 import { badRequest, serverError } from "../../helpers/http-helper"
 import { Controller, EmailValidator, HttpRequest, HttpResponse } from "../../protocols";
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor(emailValidator: EmailValidator) {
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator;
+    this.addAccount = addAccount;
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
@@ -30,6 +33,8 @@ export class SignUpController implements Controller {
       if (!isValidEmail) {
         return badRequest(new InvalidParamError('email'));
       }
+
+      const account = this.addAccount.add({ name, email, password });
 
       return {
         statusCode: 200,
