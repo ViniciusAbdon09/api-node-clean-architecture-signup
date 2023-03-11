3e503c10d76d9858602b1349739fdf0e8c71d9eb
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-03-10 23:41:46 -0300

    test: ensure BcryptAdapter return a hash on success

--- src/infra/criptograph/bcrypt-adapter.spec.ts
+++ src/infra/criptograph/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import { BcryptAdapter } from "./bcrypt-adapter";
 import bcrypt from 'bcrypt';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise(resolve => resolve('hashed'))
+  }
+}))
+
 describe('bcrypt adapter', () => {
   test('Should call bcript if correct value', async () => {
     const salt = 12;
@@ -8,5 +14,12 @@ describe('bcrypt adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+    const hashed = await sut.encrypt('any_value');
+    expect(hashed).toBe('hashed')
   })
 })
