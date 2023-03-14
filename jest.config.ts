8d9e2c80f2ed1e5c45298cf4602ea25a17fabb28
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-03-14 20:19:48 -0300

    chore: add jest configs for mongodb

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ export const configDefault = {
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "node",
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
