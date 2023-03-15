fcb99e8ec1fd7808517daff8c8a964cd849e9ef4
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-03-14 22:53:31 -0300

    refactor: move sut creation to a factory helper method

--- src/infra/db/memoryDB/accountRepository/account.spec.ts
+++ src/infra/db/memoryDB/accountRepository/account.spec.ts
@@ -1,14 +1,21 @@
 import { clientMemoryDB } from "../helpers/repository-in-memory-helper";
 import { AccountMomoryRepository } from "./account";
 
-
 describe('Account Mongo Repository', () => {
   afterAll(() => {
     clientMemoryDB.reset();
   })
 
+  beforeEach(() => {
+    clientMemoryDB.reset();
+  })
+
+  const makeSut = (): AccountMomoryRepository => {
+    return new AccountMomoryRepository();
+  }
+
   test('Should return an account on success', async () => {
-    const sut = new AccountMomoryRepository();
+    const sut = makeSut();
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
