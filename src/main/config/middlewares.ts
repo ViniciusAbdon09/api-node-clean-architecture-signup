0ddfeb85cd7060c78e15903858e7a37b9b01c856
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-03-14 23:42:50 -0300

    feat: ensure CORS is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyPaser } from '../middlewares/body-parser'
+import { cors } from '../middlewares/cors'
 
 export default (app: Express) => {
-  app.use(bodyPaser)
+  app.use(bodyPaser);
+  app.use(cors);
 }
