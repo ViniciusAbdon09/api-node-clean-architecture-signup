e8d401ec47a164766dbeeba96e8864ceb39f09c7
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-03-17 00:18:15 -0300

    feat: ensure signup route returns an account on success

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,7 @@
 import { Express } from 'express'
 import { bodyPaser, cors, contentType } from '../middlewares'
 
-export default (app: Express) => {
+export const setupMiddlewares = (app: Express) => {
   app.use(bodyPaser);
   app.use(cors);
   app.use(contentType);
