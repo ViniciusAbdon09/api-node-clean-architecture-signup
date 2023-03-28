63c0136d5c4198f1dd2e69b9d80244946f0dc561
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-03-27 22:38:54 -0300

    refactor: make serverError return the error stack

--- src/presentation/controllers/signup-controller/signup.ts
+++ src/presentation/controllers/signup-controller/signup.ts
@@ -37,8 +37,7 @@ export class SignUpController implements Controller {
 
       return ok<AccountModel>(account)
     } catch (e) {
-      console.error(e)
-      return serverError()
+      return serverError(e as Error)
     }
   }
 }
