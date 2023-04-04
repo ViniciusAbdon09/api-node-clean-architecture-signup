cfa6cfd5f2b8be36f794f73bf962921f16f7363d
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-03 23:23:58 -0300

    feat: ensure LoginController calls emailValidator if correct email

--- src/presentation/controllers/login-controller/login.ts
+++ src/presentation/controllers/login-controller/login.ts
@@ -1,8 +1,11 @@
 import { MissingParamError } from "../../error";
 import { badRequest, ok } from "../../helpers/http-helper";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
+import { EmailValidator } from "../signup-controller/signup-protocols";
 
 export class LoginController implements Controller {
+
+  constructor(private readonly emailvalidator: EmailValidator) { }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse<any>> {
     if (!httpRequest.body?.email) {
@@ -13,6 +16,7 @@ export class LoginController implements Controller {
       return Promise.resolve(badRequest(new MissingParamError('password')))
     }
 
+    this.emailvalidator.isValid(httpRequest.body.email)
     return Promise.resolve(ok({}))
   }
 }
