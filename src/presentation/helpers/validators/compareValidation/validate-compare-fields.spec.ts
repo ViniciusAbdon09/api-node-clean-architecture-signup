8755ea0d2996995b798d0aa689a0f33ede00c9ac
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-11 19:48:18 -0300

    test: add tests to compare fileds validation

--- src/presentation/helpers/validators/compareValidation/validate-compare-fields.spec.ts
+++ src/presentation/helpers/validators/compareValidation/validate-compare-fields.spec.ts
@@ -1,10 +1,20 @@
 import { InvalidParamError } from "../../../error";
 import { ValidationCompareFields } from "./validateCompareFields"
 
+const makeSut = (): ValidationCompareFields => {
+  return new ValidationCompareFields('field', 'fieldToCompare');
+}
+
 describe('Compare Validation Fields', () => {
-  test('Should return a invalidaParam error if validation fails', () => {
-    const sut = new ValidationCompareFields('pass', 'confirm');
-    const error = sut.validate({ password: 'any_pass', passwordConfirm: 'any_confirmation' });
-    expect(error).toEqual(new InvalidParamError('passwordConfirm'))
+  test('Should return a invalidaParamError if validation fails', () => {
+    const sut = makeSut();
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'round-value' });
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut();
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' });
+    expect(error).toBeFalsy()
   })
 })
