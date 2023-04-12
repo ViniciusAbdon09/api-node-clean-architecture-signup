38e7111138517d3e82e71eaa4a3e73580cfab9aa
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-12 20:03:09 -0300

    feat: ensure DbAuthentication calls hashCompare with correct values

--- src/data/useCases/authentication/db-authentication.ts
+++ src/data/useCases/authentication/db-authentication.ts
@@ -1,11 +1,19 @@
 import { Authentication, AuthenticationModel } from "../../../domain/useCases/authentication/authentication";
+import { HashComparer } from "../../protocols/criptography/hash-compare";
 import { LoadAccountByEmailRepository } from "../../protocols/db/account/load-account-by-email-repository";
 
 export class DbAuthentication implements Authentication {
-  constructor(private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) { }
+  constructor(
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashCompare: HashComparer
+  ) { }
 
   async auth(authentication: AuthenticationModel): Promise<string | null> {
-    await this.loadAccountByEmailRepository.load(authentication.email)
+    const account = await this.loadAccountByEmailRepository.load(authentication.email);
+
+    if (account) {
+      const isMatch = await this.hashCompare.compare(authentication.password, account.password)
+    }
 
     return null;
   }
