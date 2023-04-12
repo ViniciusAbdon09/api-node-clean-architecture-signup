6289fd8d76887cabcd4f794a7d4feac8fc3ac14f
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-12 20:40:24 -0300

    feat: ensure DbAuthentication calls TokenGenerate with correct id

--- src/data/useCases/authentication/db-authentication.ts
+++ src/data/useCases/authentication/db-authentication.ts
@@ -1,11 +1,13 @@
 import { Authentication, AuthenticationModel } from "../../../domain/useCases/authentication/authentication";
 import { HashComparer } from "../../protocols/criptography/hash-compare";
+import { TokenGenerate } from "../../protocols/criptography/token-generate";
 import { LoadAccountByEmailRepository } from "../../protocols/db/account/load-account-by-email-repository";
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashCompare: HashComparer
+    private readonly hashCompare: HashComparer,
+    private readonly tokenGenerate: TokenGenerate
   ) { }
 
   async auth(authentication: AuthenticationModel): Promise<string | null> {
@@ -15,6 +17,8 @@ export class DbAuthentication implements Authentication {
       const isMatch = await this.hashCompare.compare(authentication.password, account.password);
 
       if (!isMatch) return null;
+
+      return await this.tokenGenerate.generate(account.id);
     }
 
     return null;
