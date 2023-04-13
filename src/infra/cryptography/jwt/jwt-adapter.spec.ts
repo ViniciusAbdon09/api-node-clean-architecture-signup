b56e858c84500cb76455f57f91d5225c550c5d55
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-12 23:21:08 -0300

    test: ensure JwtAdapter returns a token when sign succeeds

--- src/infra/cryptography/jwt/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt/jwt-adapter.spec.ts
@@ -19,11 +19,23 @@ const makeSut = (): SutTypes => {
   }
 };
 
+jest.mock('jsonwebtoken', () => ({
+  sign: (): Promise<string> => {
+    return Promise.resolve('any_token')
+  }
+}))
+
 describe('JWT Adapter', () => {
   test('Should call sign with correct values', async () => {
     const { sut, secret } = makeSut();
     const signSpy = jest.spyOn(jwt, 'sign');
     await sut.encrypt({ id: 'any_id' });
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, secret)
+  })
+
+  test('Should return a token on sign success', async () => {
+    const { sut } = makeSut();
+    const accessToken = await sut.encrypt({ id: 'any_id' });
+    expect(accessToken).toBe('any_token')
   })
 })
