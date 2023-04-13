d01e5939b3f8f19ee6ba6bd8853679d0fd35f77c
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-12 22:13:37 -0300

    feat: ensure BcrypAdapter calls bcrypt compare with correct values

--- src/data/protocols/criptography/hash-compare.ts
+++ src/data/protocols/criptography/hash-compare.ts
@@ -1,3 +1,3 @@
 export interface HashComparer {
-  compare(password: string, hashedPassword: string): Promise<boolean>;
+  compare(value: string, hashedValue: string): Promise<boolean>;
 }
--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -1,7 +1,8 @@
 import { Hasher } from "../../data/protocols/criptography/hasher";
 import bcrypt from 'bcrypt';
+import { HashComparer } from "../../data/protocols/criptography/hash-compare";
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   private readonly salt: number
 
   constructor(salt: number) {
@@ -11,5 +12,11 @@ export class BcryptAdapter implements Hasher {
   async hash(value: string): Promise<string> {
     const hashed = await bcrypt.hash(value, this.salt);
     return hashed;
+  }
+
+  async compare(value: string, hashedValue: string): Promise<boolean> {
+    const isMatch = await bcrypt.compare(value, hashedValue);
+
+    return isMatch;
   }
 }
