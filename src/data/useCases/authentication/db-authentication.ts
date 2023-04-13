878788a967099334fd966f55180d28b66eff0477
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-12 21:06:12 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/useCases/authentication/db-authentication.ts
+++ src/data/useCases/authentication/db-authentication.ts
@@ -2,12 +2,14 @@ import { Authentication, AuthenticationModel } from "../../../domain/useCases/au
 import { HashComparer } from "../../protocols/criptography/hash-compare";
 import { TokenGenerate } from "../../protocols/criptography/token-generate";
 import { LoadAccountByEmailRepository } from "../../protocols/db/account/load-account-by-email-repository";
+import { UpdateAccessTokenRepository } from "../../protocols/db/account/update-access-token-repository";
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashCompare: HashComparer,
-    private readonly tokenGenerate: TokenGenerate
+    private readonly tokenGenerate: TokenGenerate,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) { }
 
   async auth(authentication: AuthenticationModel): Promise<string | null> {
@@ -19,6 +21,8 @@ export class DbAuthentication implements Authentication {
       if (!isMatch) return null;
 
       const accessToken = await this.tokenGenerate.generate(account.id);
+
+      await this.updateAccessTokenRepository.update(account.id, accessToken)
 
       return accessToken;
     }
