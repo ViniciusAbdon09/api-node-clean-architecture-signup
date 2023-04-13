7f6ad5d6ca00dad1f8e8c66d44e465653e74ed6b
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-13 00:31:58 -0300

    feat: ensure AccountMemoryRepository returns an account on loadByEmail success

--- src/infra/db/memoryDB/accountRepository/account.ts
+++ src/infra/db/memoryDB/accountRepository/account.ts
@@ -1,15 +1,19 @@
 import { AddAccountRepository } from "../../../../data/protocols/db/account/add-account-repository";
+import { LoadAccountByEmailRepository } from "../../../../data/useCases/authentication/db-authentication-protocols";
 import { AccountModel } from "../../../../domain/models/account";
 import { AddAccountModel } from "../../../../domain/useCases/addAccount/addAccount";
 import { clientMemoryDB } from "../helpers/repository-in-memory-helper";
 
-export class AccountMomoryRepository implements AddAccountRepository {
+export class AccountMomoryRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    clientMemoryDB.addCollection('accouts');
-
+    clientMemoryDB.addCollection('accounts');
     const result = clientMemoryDB.add<AddAccountModel, AccountModel>('accounts', accountData)
-
     return result
   }
 
+  async loadByEmail(email: string): Promise<AccountModel | null> {
+    clientMemoryDB.addCollection('accounts');
+    const result = clientMemoryDB.getByOne<AccountModel>('accounts', 'email', email);
+    return result
+  }
 }
