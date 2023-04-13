73c4427caf39eaac9504058bd4dfa2e772786213
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-13 20:33:29 -0300

    feat: ensure AccountMomoryRepository updates the account accessToken on update access token success

--- src/infra/db/memoryDB/accountRepository/account.ts
+++ src/infra/db/memoryDB/accountRepository/account.ts
@@ -1,10 +1,10 @@
 import { AddAccountRepository } from "../../../../data/protocols/db/account/add-account-repository";
-import { LoadAccountByEmailRepository } from "../../../../data/useCases/authentication/db-authentication-protocols";
+import { LoadAccountByEmailRepository, UpdateAccessTokenRepository } from "../../../../data/useCases/authentication/db-authentication-protocols";
 import { AccountModel } from "../../../../domain/models/account";
 import { AddAccountModel } from "../../../../domain/useCases/addAccount/addAccount";
 import { clientMemoryDB } from "../helpers/repository-in-memory-helper";
 
-export class AccountMomoryRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AccountMomoryRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     clientMemoryDB.addCollection('accounts');
     const result = clientMemoryDB.add<AddAccountModel, AccountModel>('accounts', accountData)
@@ -15,5 +15,12 @@ export class AccountMomoryRepository implements AddAccountRepository, LoadAccoun
     clientMemoryDB.addCollection('accounts');
     const result = clientMemoryDB.getByOne<AccountModel>('accounts', 'email', email);
     return result
+  }
+
+  async updateAccessToken(id: string, token: string): Promise<void> {
+    clientMemoryDB.addCollection('accounts');
+    const account = clientMemoryDB.getByOne<AccountModel>('accounts', 'id', id);
+    if (!account) return;
+    clientMemoryDB.update<AccountModel>('accounts', { ...account, accessToken: token })
   }
 }
