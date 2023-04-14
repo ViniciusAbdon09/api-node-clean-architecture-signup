c69e18f82952fd5fc51af385c5f1132bd414eca0
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-13 21:37:53 -0300

    feat: add login route

--- src/main/routes/login.routes.ts
+++ src/main/routes/login.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { adapteRoute } from '../adapters/express-routes-adapter';
-import { makeSignupController } from '../factories/signup'
+import { makeSignupController } from '../factories/signup';
+import { makeLoginController } from '../factories/login';
 
 export default (router: Router): void => {
   router.post('/signup', adapteRoute(makeSignupController()))
+  router.post('/login', adapteRoute(makeLoginController()))
 }
