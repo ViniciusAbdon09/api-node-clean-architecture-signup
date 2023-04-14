46cc3bdc1eede72fd2d769829261585fc6163e75
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-13 21:26:17 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,4 @@
 export default {
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'jWt2312=(?;.LS1@!1=='
 }
--- src/main/factories/login/index.ts
+++ src/main/factories/login/index.ts
@@ -1,4 +1,21 @@
+import { DbAuthentication } from "../../../data/useCases/authentication/db-authentication";
+import { BcryptAdapter } from "../../../infra/cryptography/bcrypt/bcrypt-adapter";
+import { JwtAdapter } from "../../../infra/cryptography/jwt/jwt-adapter";
+import { AccountMomoryRepository } from "../../../infra/db/memoryDB/accountRepository/account-memory-repository";
+import { LogMomoryRepository } from "../../../infra/db/memoryDB/logRepository/log-memory-repository";
+import { LoginController } from "../../../presentation/controllers/login-controller/login";
+import { Controller } from "../../../presentation/protocols";
+import { LogControllerDecorator } from "../../decorators/logs";
+import { makeLoginValidation } from "./loginValidation-factory";
+import env from '../../config/env';
 
-export const makeLoginController = () => {
-
+export const makeLoginController = (): Controller => {
+  const accountMemoryRepository = new AccountMomoryRepository();
+  const salt = 12;
+  const bcryptAdapter = new BcryptAdapter(salt);
+  const jwtAdapter = new JwtAdapter(env.jwtSecret);
+  const dbAuthentication = new DbAuthentication(accountMemoryRepository, bcryptAdapter, jwtAdapter, accountMemoryRepository);
+  const loginController = new LoginController(makeLoginValidation(), dbAuthentication);
+  const logErrorRepository = new LogMomoryRepository();
+  return new LogControllerDecorator(loginController, logErrorRepository);
 }
