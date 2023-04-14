ae314c781e5213ca394cbc6708e6f26fa3025e1d
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-13 21:44:52 -0300

    test: login router return 401 if invalid credentials

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -47,5 +47,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     });
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid@email.com',
+          password: '123123',
+        })
+        .expect(401)
+    });
   });
 })
