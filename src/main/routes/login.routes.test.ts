9a7dd48cb86c105a8b3c9025acccec9c80851168
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-13 21:38:04 -0300

    test: add login route

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -1,4 +1,7 @@
+import { hash } from 'bcrypt';
 import request from 'supertest';
+import { AccountModel } from '../../domain/models/account';
+import { AddAccountModel } from '../../domain/useCases/addAccount/addAccount';
 import { clientMemoryDB } from '../../infra/db/memoryDB/helpers/repository-in-memory-helper';
 import { app } from '../config/app';
 
@@ -20,6 +23,27 @@ describe('Login Routes', () => {
           email: 'valid@email.com',
           password: '123123',
           passwordConfirm: '123123'
+        })
+        .expect(200)
+    });
+  });
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      clientMemoryDB.addCollection('accounts');
+      const passHash = await hash('123123', 12);
+      clientMemoryDB.add<AddAccountModel, AccountModel>('accounts',
+        {
+          name: 'valid name',
+          email: 'valid@email.com',
+          password: passHash,
+        });
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid@email.com',
+          password: '123123',
         })
         .expect(200)
     });
