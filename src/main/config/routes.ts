21e2cbc168e083ad76793db64ccf1f3a147f2ff6
Vinicius Abdon <vinicius.alves39@fatec.sp.gov.br>
2023-04-14 19:42:19 -0300

    refactor: change routes to use fileSystem insted of fasglob

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,9 +1,14 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob';
+import { readdirSync } from 'fs';
 
 export const setupRoutes = (app: Express) => {
   const router = Router();
   app.use('/api', router);
 
-  fg.sync('**/src/main/routes/**.routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
+  // importando os arquivos de rota dinamicamente
+  readdirSync(`${__dirname}/../routes`).map(async file => {
+    if (!file.includes('.test.')) {
+      (await import(`../routes/${file}`)).default(router)
+    }
+  })
 }
